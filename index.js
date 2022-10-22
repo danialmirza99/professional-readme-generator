653d909453c73215152c10476f85bcb6c0e8ba87
danialmirza99 <danialmirza99@gmail.com>
2022-10-21 21:55:00 -0700

    Basic readme generated

--- index.js
+++ index.js
@@ -4,14 +4,43 @@ const fs = require('fs');
 // TODO: Create an array of questions for user input
 const questions = [];
 
+const writeReadme = (response) =>
+         
+`# Title: ${response.title}
 
+## Description
+
+${response.description}
+
+## Installation
+
+${response.installation}
+
+## Usage
+
+${response.usage}
+
+## License
+
+## Contributing
+
+${response.contributing}
+
+## Tests
+
+${response.testing}
+
+## Questions`;
+        
 
 // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {
-    const writeReadme = (response) =>
-        ` #${response.title}
-        `
-// }
+function writeToFile(fileName, data) {
+    
+    
+        fs.writeFile(fileName, data, (err) =>
+        err ? console.log(err) : console.log('Successfully created readme.md!')
+    );
+}
 
 // TODO: Create a function to initialize app
 function init() {
@@ -21,14 +50,36 @@ function init() {
         type: 'input',
         name: 'title',
         message: 'What is the title of your repository/project?',
-      }
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a description about your repository/project.',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'Please enter any instructions you may have for installation.',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Please write a usage manual',
+      },
+      {
+        type: 'input',
+        name: 'contributing',
+        message: 'Please enter contribution guidelines.',
+      },
+      {
+        type: 'input',
+        name: 'testing',
+        message: 'Please enter testing instructions.',
+      },
     ])
     .then((response) => {
       const readmeGen = writeReadme(response);
-  
-      fs.writeFile('test.md', readmeGen, (err) =>
-        err ? console.log(err) : console.log('Successfully created readme.md!')
-      );
+      writeToFile('./utils/README.md', readmeGen);
     });
 }
 
---
+++ utils/README.md
@@ -0,0 +1,25 @@
+# Title: Test
+
+## Description
+
+My description
+
+## Installation
+
+My instructions
+
+## Usage
+
+My usage
+
+## License
+
+## Contributing
+
+My constribution
+
+## Tests
+
+My testing guidlines
+
+## Questions
